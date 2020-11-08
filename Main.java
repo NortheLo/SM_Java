611e2401ccc60707b1f34461a0ab5e34940c7af8
louis <louis@northe-online.de>
2020-11-08 16:41:12 +0100

    target added in main

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 import java.util.Vector;
 public class Main {
     public static void main(String[] args) {
+        String target = "https://yopad.eu/p/JavaProjektTestTermine-365days/export/txt";
+        Document document = new Document(target);
+        
         System.out.println("Hello World");
         Parser parser = new Parser();
         Vector<Termin> termine = parser.parse("C://Users/jonas/Documents/SM_Java/sample.txt");
